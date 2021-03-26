7c6e7453c4a9885572d4285ce403fe50087254a5
Murray <jmurraylouw@gmail.com>
2021-03-26 15:23:31 +0200

    Moved code to function to generate waypoints

--- initialize/initialize_quad_simulation.m
+++ initialize/initialize_quad_simulation.m
@@ -56,13 +56,8 @@ end
 
 %% Waypoints
 
+threshold = 1e-3; % Threshold to reach waypoint, for threshold mode
 num_waypoints = 100; % Number of waypoints
-waypoints = zeros(num_waypoints,3); % Initialize empty matrix
-waypoint_time = zeros(num_waypoints,1); % Vector of time between waypoints. If time < 0: wait till reach threshhold before next waypoint
-
-minmax = @(x, min_x, max_x) max(min(x,max_x),min_x); % function to restrict input x between min and max values
-waypoint_start = [0, 0, 10]; % Starting waypoint [x,y,z] (z is up positive for now)
-waypoints(1,:) = waypoint_start;
 
 waypoint_max = [15, 15, 20]; % Max values in waypoint [x,y,z]
 waypoint_min = [-15, -15, 10]; % Min values in waypoint [x,y,z]
@@ -70,16 +65,11 @@ waypoint_min = [-15, -15, 10]; % Min values in waypoint [x,y,z]
 step_max = [5, 2, 2]; % Max step/change in waypoint [x,y,z]
 step_min = [0, 0, 0]; % Min step/change in waypoint [x,y,z]
 
-time_max = 30; % Min time between waypoints
-time_min = 5; % Max time between waypoints
+time_max = 30; % Min time between waypoints (s)
+time_min = 5; % Max time between waypoints (s)
 
-rng(0); % Initialise random number generator for repeatability
-for i = 2:num_waypoints % Populate waypoint matrix
-    waypoint_step = ((step_max - step_min).*rand(1,3) + step_min).*sign(randn(1,3)); % Step size to next waypoint [x,y,z]
-    waypoints(i,:) = waypoints(i-1,:) + waypoint_step; % Generate next waypoint
-    waypoints(i,:) = minmax(waypoints(i,:), waypoint_min, waypoint_max); % Limit waypoints to within min and max range
-    waypoint_time(i) = floor(((time_max - time_min).*rand() + time_min)); % Time interval between waypoints
-end
+rng_seed = 0;
+[waypoints, waypoints_time] = random_waypoints(num_waypoints, step_min, step_max, waypoint_min, waypoint_max, time_min, time_max, rng_seed);
 
 % Manual waypoints:
 % waypoints = [ ...
@@ -90,9 +80,6 @@ end
 %     5;
 %     5;
 %     5];
-
-waypoints(:,3) = -waypoints(:,3); % Convert z to down-positive
-threshold = 1e-3; % Threshold to reach waypoint
 
 % plot(cumsum(waypoint_time),waypoints)
 % title('waypoints')
@@ -105,20 +92,42 @@ initialize_inputs;
 %% Step response
 initialize_step 
 
-%% Run simulation
-%quad_sim_play;
-
-% if enable_payload
-%    sim quad_simulation_with_payload_lqr.slx;
-% else
-%     sim quad_simulation.slx;
-% end
-% 
-%sim('quad_simulation_with_payload_lqr')
 
 % Test comment from laptop
+function [waypoints, waypoints_time] = random_waypoints(num_waypoints, step_min, step_max, waypoint_min, waypoint_max, time_min, time_max, rng_seed)
+%% Returns matrix with a random waypoints per row, and random time intervals between waypoints
 
+% num_waypoints = Number of waypoints
 
+% waypoint_max = Max values in waypoint [x,y,z]
+% waypoint_min = Min values in waypoint [x,y,z]
+% 
+% step_max = Max step/change in waypoint [x,y,z]
+% step_min = Min step/change in waypoint [x,y,z]
+% 
+% time_max = Min time between waypoints (s)
+% time_min = Max time between waypoints (s)
+
+    minmax = @(x, min_x, max_x) max(min(x,max_x),min_x); % anonymous function to restrict input x between min and max values
+
+    rng(rng_seed); % Initialise random number generator for repeatability
+    
+    waypoints = zeros(num_waypoints,3); % Initialize empty matrix
+    waypoint_time = zeros(num_waypoints,1); % Vector of time between waypoints. If time < 0: wait till reach threshhold before next waypoint
+
+    waypoint_start = [0, 0, 10]; % Starting waypoint [x,y,z] (z is up positive for now)
+    waypoints(1,:) = waypoint_start;
+    
+    for i = 2:num_waypoints % Populate waypoint matrix
+        waypoint_step = ((step_max - step_min).*rand(1,3) + step_min).*sign(randn(1,3)); % Step size to next waypoint [x,y,z]
+        waypoints(i,:) = waypoints(i-1,:) + waypoint_step; % Generate next waypoint
+        waypoints(i,:) = minmax(waypoints(i,:), waypoint_min, waypoint_max); % Limit waypoints to within min and max range
+        waypoint_time(i) = floor(((time_max - time_min).*rand() + time_min)); % Time interval between waypoints
+    end
+
+    waypoints(:,3) = -waypoints(:,3); % Convert z to down-positive
+    
+end
 
 
 
