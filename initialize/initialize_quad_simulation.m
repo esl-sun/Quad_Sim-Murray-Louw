4402bdca24043de4d7d055b397b20c9823757299
Murray Louw <jmurraylouw@gmail.com>
2021-04-29 12:23:01 +0200

    moved mpc to seperate referenced model to run in Accelerator mode. Improved simulation speed from 15s to 9s for Stoptime = 5s

--- initialize/initialize_quad_simulation.m
+++ initialize/initialize_quad_simulation.m
@@ -2,7 +2,7 @@
 enable_payload = 1;
 
 %% Simulation constants
-sim_time = 100;
+sim_time = 5;
 sim_freq = 500; % Used for sample time of blocks and fixed step size of models
 
 %% Simulation Folder Setup
@@ -94,11 +94,11 @@ initialize_inputs;
 initialize_step 
 
 %% Run simulation
-% tic;
-% disp('Start simulation.')
-% sim 'quad_simulation_with_payload.slx'
-% disp('Execution time:')
-% toc
+tic;
+disp('Start simulation.')
+sim 'quad_simulation_with_payload.slx'
+disp('Execution time:')
+toc
 
 
 
