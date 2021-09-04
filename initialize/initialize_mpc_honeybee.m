7aa60bf814f4fbf00485159bc88111e03a6bff9d
Murray Louw <jmurraylouw@gmail.com>
2021-09-04 17:50:56 +0200

    Added parallel integrator and derivative controller to illiminate steady-state error

--- initialize/initialize_mpc_honeybee.m
+++ initialize/initialize_mpc_honeybee.m
@@ -87,6 +87,10 @@ mpc_sys.InputGroup.MV = 1:nu; % Munipulated Variable indices
 tuning_weight = 1; % Tuning weight for mv and mv rate together. Smaller = robust, Larger = aggressive
 mo_weight = 1; % Scale all MV
 
+par_control_weight = 1; % Weight of parallel controller
+integrator_gain = 0.1 * par_control_weight; % Integrator gain of controller added in parallel
+derivative_gain = 0.1 * par_control_weight; % Derivative gain of controller added in parallel
+
 vel_weight = 1; % Velocity tracking weight
 theta_weight = 0; % Payload swing angle. Larger = less swing angle, Smaller = more swing
 dtheta_weight = 4; % Derivative of Payload swing angle
