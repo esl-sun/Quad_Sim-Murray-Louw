ce1cb51f5d93c96c6a41d7e3603104427ea92c06
jmurraylouw <jmurraylouw@gmail.com>
2021-09-15 11:41:23 +0200

    Added rootlocus plots

--- plots/rootlocus_to_csv.m
+++ plots/rootlocus_to_csv.m
@@ -8,30 +8,32 @@ D = D_vn;
 
 [G_NUM, G_DENOM] = tfdata(G, 'v');
 
-% clear zero;
-% clear zeros; % Clear any zero variables masking the function
-% [ctrl_zeros, ctrl_gain] = zero(D*s^0);
-% p_gain = 0;
-% i_gain = 0;
-% d_gain = 0;
-% if isempty(ctrl_zeros)
-%     p_gain =  ctrl_gain;
-% elseif length(ctrl_zeros) == 1
-%     p_gain = ctrl_gain;
-%     i_gain = p_gain * abs(ctrl_zeros(1));
-% elseif length(ctrl_zeros) == 2
-%     d_gain = ctrl_gain;
-%     p_gain = d_gain*(abs(ctrl_zeros(1)) + abs(ctrl_zeros(2)));
-%     i_gain = d_gain*abs(ctrl_zeros(1))*abs(ctrl_zeros(2));
-% end
+GD = G*D;
 
-Gol = G*D;
+%% Plot open loop
+figure
+rlocus(G)
+hold on
+r_ol = rlocus(G,1); % Get poles for gain of k = 1
+plot(real(r_ol), imag(r_ol), 'rs')
+ylim([-4 4])
+xlim([-5 0.2])
+% In Figure GUI: Save as .eps
 
+%% Plot closed loop
+figure
+rlocus(GD)
+hold on
+r_control = rlocus(GD,1); % Get poles for gain of 1
+plot(real(r_control), imag(r_control), 'rs')
+ylim([-4 4])
+xlim([-5 0.2])
+stop
 % Root locus data
 [r_plant, gain_plant] = rlocus(G); % Poles and zeros of open loop system
 [r, gain] = rlocus(Gol); % Poles and zeros of closed loop system
 [Z, K] = zero(Gol); % Gain K
-stop
+
 % Obtain real and imaginary parts
 % Plant
 rl_plant = zeros(length(r_plant), 2*min(size(r_plant)));
@@ -73,25 +75,26 @@ for i=1:min(size(r))
     rl_names_cl(2*(min(size(r))+i)) = "cli" + i; 
 end
 
-filename_plant = "controller_design/rlocus_plant.csv";
-filename_design = "controller_design/rlocus_design.csv";
+filename_plant = "plots/rootlocus/rootlocus_ol.csv";
+filename_design = "plots/rootlocus/rootlocus_cl.csv";
 writematrix([rl_names; rl_plant], filename_plant);
 writematrix([rl_names_cl; rl_ol], filename_design);
 
-% Step response
-step_sim_time = 5*15;
-step_sim_freq = 250;
-step_time = 1;
-
-disturb = 0;
-disturb_time = 8;
-disturb_val = 0.01;
-
-log_start_time = 0.1;
-log = step_time*step_sim_freq - log_start_time*step_sim_freq;
-
-ctrl = sim("controller_design/controller_design_step.slx");
-
-dwn_smple = 15;
-filename_step = "controller_design/rstep.csv";
-writematrix(["time", "input", "p", "pi", "pid"; downsample(ctrl.ctrl.time(log:end), dwn_smple), downsample(ctrl.ctrl.data(log:end,1), dwn_smple), downsample(ctrl.ctrl.data(log:end,2), dwn_smple), downsample(ctrl.ctrl.data(log:end,3), dwn_smple), downsample(ctrl.ctrl.data(log:end,4), dwn_smple)], filename_step);
+plot(rl_plant, '.')
+% % Step response
+% step_sim_time = 5*15;
+% step_sim_freq = 250;
+% step_time = 1;
+% 
+% disturb = 0;
+% disturb_time = 8;
+% disturb_val = 0; % 0.01;
+% 
+% log_start_time = 0.1;
+% log = step_time*step_sim_freq - log_start_time*step_sim_freq;
+% 
+% ctrl = sim("controller_design/controller_design_step.slx");
+% 
+% dwn_smple = 15;
+% filename_step = "controller_design/rstep.csv";
+% writematrix(["time", "input", "p", "pi", "pid"; downsample(ctrl.ctrl.time(log:end), dwn_smple), downsample(ctrl.ctrl.data(log:end,1), dwn_smple), downsample(ctrl.ctrl.data(log:end,2), dwn_smple), downsample(ctrl.ctrl.data(log:end,3), dwn_smple), downsample(ctrl.ctrl.data(log:end,4), dwn_smple)], filename_step);
